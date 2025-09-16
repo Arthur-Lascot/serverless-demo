8ab4ed7fa515c0bd27a4f937c46e0b79f7f8eb8c
arthur.oldrati <arthur.oldrati@epita.fr>
2025-09-16 10:44:53 +0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_lambda_function" "api" {
   }
 }
 
-resource "aws_dynamodb_table" "contacts" {
+data "aws_dynamodb_table" "contacts" {
   name         = "contacts"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
